e00eb3741e6466a8f42fc203db8d26a7432ba7dd
Hao Ding <181220010@smail.nju.edu.cn>
2022-04-21 21:25:34 +0800

    no longer maintain this repo

--- install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get update
-sudo apt-get install ffmpeg libsm6 libxext6 nodejs -y --upgrade
-python -m pip install --upgrade pip
-python -m pip install requests PyExecJS python-dotenv pytz ddddocr --upgrade
--- njupass/ocr.py
+++
@@ -1,8 +0,0 @@
-import ddddocr
-
-
-def detect(input):
-    ocr = ddddocr.DdddOcr(show_ad=0)
-    with input as f:
-        res = ocr.classification(f.read())
-    return res
